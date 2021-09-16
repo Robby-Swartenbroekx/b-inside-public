0731d66a08f4065de4b3896e3ee46fb24a644a88
Robby Swartenbroekx <robbys@b-inside.be>
2021-09-16 15:32:34 +0200

    Changed script to only allow outgoing connections

---
+++ Teamviewer No Incoming Connections/Teamviewer No Incoming Connections.ps1
@@ -0,0 +1,27 @@
+$TeamViewerRegPath = if (Test-Path "HKLM:Software\TeamViewer") { "HKLM:Software\TeamViewer" }
+elseif (Test-Path "HKLM:Software\Wow6432Node\TeamViewer") { "HKLM:Software\Wow6432Node\TeamViewer" }
+else { "No TeamViewer Installed" }
+
+if ($TeamViewerRegPath -eq "No TeamViewer Installed") {
+    $RandomPassword = "Disabled" # Bogus Data
+    $FixedPassword = "Disabled" # Bogus Data
+}
+
+$RandomPassword = if ((Get-ItemProperty $TeamViewerRegPath).Security_PasswordStrength -eq 3) { "Disabled" } else {
+    New-ItemProperty -Path $TeamViewerRegPath -Name "Security_PasswordStrength" -Value 3 -PropertyType DWORD -Force | Out-Null
+    Restart-Service -Name "Teamviewer" -Force | Out-Null
+    "Enabled"
+}
+$FixedPassword = if ($null -eq (Get-ItemProperty $TeamViewerRegPath).PermanentPassword) { 
+    Remove-ItemProperty -Path $TeamViewerRegPath -Name "PermanentPassword" -Force | Out-Null
+    Restart-Service -Name "Teamviewer" -Force | Out-Null
+    "Enabled"
+}
+else { "Disabled" }
+
+#region Output Values
+"Registry Path   : $TeamViewerRegPath"
+"Random Password : $RandomPassword"
+"Fixed Password  : $FixedPassword"
+#endregion
+
